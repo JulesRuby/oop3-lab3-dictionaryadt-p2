deb2050c55de0722a3a9800e3c8b842b37d0a6dd
JulesRuby <JulesRuby@users.noreply.github.com>
2025-10-31 14:19:08 -0600

    Editted documentation (did I need to?) Implemented @Override methods create() and insert()

--- src/implementations/Dictionary.java
+++ src/implementations/Dictionary.java
@@ -15,7 +15,7 @@ public class Dictionary<K,V> implements DictionaryADT<K,V>
 	private ArrayList<V> values;
 	
 	/**
-	 * Default constructor - creates a Dictionary with default size.
+	 * Default constructor - creates a Dictionary using DEFAULT_SIZE constant.
 	 */
 	public Dictionary()
 	{
@@ -23,25 +23,59 @@ public class Dictionary<K,V> implements DictionaryADT<K,V>
 	}
 
 	/**
-	 * Constructor with specified initial capacity.
+	 * Constructor overload with capacity argument.
 	 *
-	 * @param size The initial capacity of the dictionary
+	 * @param size The initialized capacity of the dictionary
 	 */
 	public Dictionary(int size)
 	{
 		create(size);
 	}
 
+	/**
+	 * Constructor which creatde a new Dictionary object, and initializes the
+	 * counterpart ArrayLists representing keys:values.
+	 *
+	 * Precondition: None.
+	 *
+	 * Postcondition: A dictionary object is created and its size is
+	 * initialized to the value in the argument, or a default of 10.
+	 *
+	 * @param size The initialized capacity of the dictionary
+	 */
 	@Override
-	public void create(int size) {
-		// TODO Auto-generated method stub
-		
+	public void create(int size)
+	{
+		keys = new ArrayList<K>(size);
+		values = new ArrayList<V>(size);
 	}
 
+	/**
+	 * Mutator method to insert a new key:value pair into the Dictionary.
+	 *
+	 * Precondition: Dictionary object exists, and arguments must not be null.
+	 *
+	 * Postcondition: Key:value pair is added to the Dictionary.
+	 *
+	 * @param key The key 
+	 * @param value The associated value
+	 * 
+	 * @return true if key:value pair has been added successfully
+	 * 
+	 * @throws DuplicateKeyException if the key already exists in the dictionary
+	 */
 	@Override
-	public boolean insert(K key, V value) throws DuplicateKeyException {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean insert(K key, V value) throws DuplicateKeyException 
+	{
+		if (keys.contains(key))
+		{
+			throw new DuplicateKeyException("Key already exists in the dictionary: " + key);
+		}
+		
+		keys.add(key);
+		values.add(value);
+		
+		return true;
 	}
 
 	@Override
